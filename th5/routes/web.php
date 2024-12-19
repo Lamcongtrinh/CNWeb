bc55639ddd45bee5d4c446441c3b2ccaf51d0c46
Lamcongtrinh <lamtrinh2132k4@gmail.com>
2024-12-19 15:03:09 +0700

    update

--- th5/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\IssueController;
-
-// Tạo tất cả các route cho resource 'issues'
-Route::resource('issues', IssueController::class);
